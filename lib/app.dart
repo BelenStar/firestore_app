b9e1b1364656638bcdc1fd1a6b0010c3418809fc
Nellyannis Ramirez <nfrv04@gmail.com>
2023-05-10 20:37:01 -0400

    add new signIn route

--- lib/app.dart
+++ lib/app.dart
@@ -7,12 +7,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInPage(),
     );
   }
 }
