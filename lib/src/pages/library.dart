fd12723fd7dcd4b3eedba8660c0980632d8e50bf
Nellyannis Ramirez <nfrv04@gmail.com>
2023-05-29 19:23:21 -0400

    feature($pages):minor corrections

--- lib/src/pages/library.dart
+++ lib/src/pages/library.dart
@@ -10,7 +10,7 @@ class LibraryPage extends StatefulWidget {
 class _LibraryPageState extends State<LibraryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text('BUUUU'),
     );
   }
