f96d92c5f7df2ae95d0b5ee6e4b60d5adc3636db
Nellyannis Ramirez <nfrv04@gmail.com>
2023-06-01 01:46:17 -0400

    feature($library): create toJson map method

--- lib/src/models/library_item.dart
+++ lib/src/models/library_item.dart
@@ -4,4 +4,14 @@ class Book {
   final double rating;
 
   Book({required this.name, required this.genre, required this.rating});
+
+  /* factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      name: json['name'],
+      genre: json['genre'],
+      rating: json['rating'],
+    );
+  } */
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'genre': genre, 'rating': rating};
 }
