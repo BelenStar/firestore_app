9da390f00b395fcfcb9b2e6d28facb45bcff3416
Nellyannis Ramirez <nfrv04@gmail.com>
2023-06-02 03:09:14 -0400

    feature($library): create get_books function

--- lib/src/providers/book_provider.dart
+++ lib/src/providers/book_provider.dart
@@ -3,6 +3,12 @@ import 'package:firestone_app/src/models/library_item.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LibraryProvider extends ChangeNotifier {
+  Stream<List<Book>> getBooks() => FirebaseFirestore.instance
+      .collection('books')
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Book.fromJson(doc.data())).toList());
+
   Future createBook(Book book) async {
     await FirebaseFirestore.instance.collection('books').add(book.toJson());
   }
