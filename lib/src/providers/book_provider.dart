b310d3c95626f60e357c859ff336825af2202a96
Nellyannis Ramirez <nfrv04@gmail.com>
2023-06-05 00:00:30 -0400

    feature($library):relate user id with new and old books

--- lib/src/providers/book_provider.dart
+++ lib/src/providers/book_provider.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firestone_app/src/models/library_item.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LibraryProvider extends ChangeNotifier {
+  final user = FirebaseAuth.instance.currentUser?.uid;
+
   Stream<List<Book>> getBooks() => FirebaseFirestore.instance
       .collection('books')
+      .where('user_id', isEqualTo: user)
       .snapshots()
       .map((snapshot) =>
           snapshot.docs.map((doc) => Book.fromJson(doc.data())).toList());
