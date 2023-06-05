2b371f14d6b87aad88e0755ff8122b3c6eb44013
Nellyannis Ramirez <nfrv04@gmail.com>
2023-06-04 23:58:10 -0400

    feature($library):add user as model atribute

--- lib/src/models/library_item.dart
+++ lib/src/models/library_item.dart
@@ -2,16 +2,22 @@ class Book {
   final String name;
   final String genre;
   final double rating;
+  final String? user;
 
-  Book({required this.name, required this.genre, required this.rating});
+  Book(
+      {required this.user,
+      required this.name,
+      required this.genre,
+      required this.rating});
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(
+      user: json['user_id'],
       name: json['name'],
       genre: json['genre'],
       rating: json['rating'],
     );
   }
   Map<String, dynamic> toJson() =>
-      {'name': name, 'genre': genre, 'rating': rating};
+      {'name': name, 'genre': genre, 'rating': rating, 'user_id': user};
 }
