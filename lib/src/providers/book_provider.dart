43a18c60a59be1d75e26059dbcc4c2544780e496
Nellyannis Ramirez <nfrv04@gmail.com>
2023-06-08 03:10:58 -0400

    feature($library): add update function

--- lib/src/providers/book_provider.dart
+++ lib/src/providers/book_provider.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LibraryProvider extends ChangeNotifier {
   final user = FirebaseAuth.instance.currentUser?.uid;
+  final _book = FirebaseFirestore.instance;
 
   Stream<List<Book>> getBooks() => FirebaseFirestore.instance
       .collection('books')
@@ -15,5 +16,13 @@ class LibraryProvider extends ChangeNotifier {
 
   Future createBook(Book book) async {
     await FirebaseFirestore.instance.collection('books').add(book.toJson());
+  }
+
+  Future editBook(Book book) async {
+    await _book.collection('books').doc(book.id).update(book.toJson());
+    /* final updatedBook = FirebaseFirestore.instance.collection('books').doc();
+
+    updatedBook.update(
+        {'name': book.name, 'genre': book.genre, 'rating': book.rating}); */
   }
 }
