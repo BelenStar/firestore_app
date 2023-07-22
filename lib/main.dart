12fe35cc0411384195fe4e73b525c3f8ec1ba19f
Nellyannis Ramirez <nfrv04@gmail.com>
2023-07-21 20:58:59 -0400

    feature($notifications): Add push notification and in-app messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
+// ignore_for_file: avoid_print
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firestone_app/app.dart';
+import 'package:firebase_app_installations/firebase_app_installations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  void getInstallationId() async {
+    String installationId = await FirebaseInstallations.instance.getId();
+
+    print("Installation ID: $installationId");
+  }
+
+  getInstallationId();
+
+  String? token = await messaging.getToken();
+  print('FirebaseMessaging token: $token');
+
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
   runApp(const MyApp());
 }
